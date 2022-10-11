9f126c1490a6f40fb4829d1b25f940222c7da92e
Fernando Galvan <n.brighter00@gmail.com>
2022-10-11 16:26:59 -0500

    adding styling and nav components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-        
+    <div className='navbar'>
+        <div className="logo">
+          <h2>TENNIS MAJORS</h2>
+        </div>
     </div>
   )
 }
