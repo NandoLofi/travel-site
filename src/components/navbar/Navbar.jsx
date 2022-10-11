04ed6b2fd38471d50f2b7e215322327290627679
Fernando Galvan <n.brighter00@gmail.com>
2022-10-11 17:15:01 -0500

    added icons and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function Navbar() {
   return (
     <div className='navbar'>
         <div className="logo">
           <h2>TENNIS MAJORS</h2>
+        </div>
+        <ul className="nav__menu">
+          <li>Home</li>
+          <li>Destinations</li>
+          <li>Travel</li>
+          <li>Views</li>
+        </ul>
+        <div className="nav__icons">
+          <SearchIcon className='icon'/>
+          <PersonIcon className='icon'/>
         </div>
     </div>
   )
