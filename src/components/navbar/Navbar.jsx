525680010b25f71a5a6f1c1b1424c3ff51becdc6
Fernando Galvan <n.brighter00@gmail.com>
2022-10-11 18:22:12 -0500

    added nav bar for mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
+import MenuIcon from '@mui/icons-material/Menu';
+import './Navbar.css'
 
 export default function Navbar() {
   return (
@@ -17,6 +19,25 @@ export default function Navbar() {
         <div className="nav__icons">
           <SearchIcon className='icon'/>
           <PersonIcon className='icon'/>
+        </div>
+        <div className="hamburger">
+          <MenuIcon className='icon' />
+        </div>
+        <div className="mobile__menu">
+          <ul className="mobile__nav">
+            <li>Home</li>
+            <li>Destinations</li>
+            <li>Travel</li>
+            <li>Views</li>
+          </ul>
+        </div>
+        <div className="mobile__menu__button">
+          <div className="menu__icons">
+
+          </div>
+          <div className="social__icons">
+            
+          </div>
         </div>
     </div>
   )
