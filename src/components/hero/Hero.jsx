d962c5a6aec75405098e27e0fa7cced8fc7fbf60
Fernando Galvan <n.brighter00@gmail.com>
2022-10-16 14:28:26 -0500

    added styling

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import Video from '../../assets/overhead.mp4'
-
+import SportsTennisIcon from '@mui/icons-material/SportsTennis';
 export default function Hero() {
   return (
     <div className='hero'>
@@ -12,6 +12,14 @@ export default function Hero() {
         <div className="content">
             <h1>Plan Your Next Trip to A Major</h1>
             <h2>Explore more than the matches</h2>
+            <form action="" className="form">
+                <div>
+                    <input type="text" name="search" placeholder='Search City or Open'/>
+                </div>
+                <div>
+                    <button><SportsTennisIcon className='icon'/></button>
+                </div>
+            </form>
         </div>
     </div>
   )
