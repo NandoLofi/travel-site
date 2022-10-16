d68f37e7d1f62aa9e26c521841622b604b36a628
Fernando Galvan <n.brighter00@gmail.com>
2022-10-16 12:58:02 -0500

    added files

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Hero.css'
+import Video from '../../assets/overhead.mp4'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <video autoPlay loop muted id="video">
+            <source src={Video} type='video/mp4'/>
+        </video>
+        <div className="overlay"></div>
+    </div>
   )
 }
