99df6524191d537df9594faa3da686734598d616
Fernando Galvan <n.brighter00@gmail.com>
2022-10-19 13:23:17 -0500

    added form, researching api to get functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Destinations from "./components/destinations/Destinations";
+import Search from "./components/search/Search";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Destinations/>
+      <Search />
     </div>
   );
 }
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Search.css'
+
+export default function Search() {
+  return (
+    <div className='search'>
+        <div className="container">
+            <div className='left'>
+                <h2>Enjoy the city and world-class matches!</h2>
+                <p>While experiencing ambiance of a Tennis Major, enjoy the city that host these memorable tournaments. You can visit the Louvre in Paris, enjoy NY Style pizza first hand while in New York City, some delicate seafood in Melbourne, or even some classica fish and chips in London.</p>
+            </div>
+            <div className="right">
+                <div className="promo">
+                    <h4 className='save'>Search Destinations with Dates Below</h4>
+                </div>
+                <form>
+                    <div className="input__wrap">
+                        <label>Destination</label>
+                        <select name="destination-search" id="destination">
+                            <option value="1">Paris</option>
+                            <option value="2">London</option>
+                            <option value="3">Melbourne</option>
+                            <option value="4">New York City</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
