e18114cb427a9b443c534163fc0b00bb6000f0f3
NandoLofi <n.brighter00@gmail.com>
2022-12-09 18:13:56 -0600

    basic styling to search bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,56 +1,56 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import MenuIcon from '@mui/icons-material/Menu';
-import './Navbar.css'
-import { Facebook, Instagram, Twitter, YouTube } from '@mui/icons-material';
-import { useState } from 'react';
-import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import MenuIcon from "@mui/icons-material/Menu";
+import "./Navbar.css";
+import { Facebook, Instagram, Twitter, YouTube } from "@mui/icons-material";
+import { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Navbar() {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-  const handleNav = ()=> {
-    setNav(!nav)
-  }
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
-    <div className={nav ? 'navbar nav__bg': 'navbar'}>
-        <div className="logo">
-          <h2>TENNIS MAJORS</h2>
-        </div>
-        <ul className="nav__menu">
+    <div className={nav ? "navbar nav__bg" : "navbar"}>
+      <div className="logo">
+        <h2>TENNIS MAJORS</h2>
+      </div>
+      <ul className="nav__menu">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Views</li>
+      </ul>
+      <div className="nav__icons">
+        <SearchIcon className="icon" style={{ marginRight: "1rem" }} />
+        <PersonIcon className="icon" />
+      </div>
+      <div className="hamburger" onClick={handleNav}>
+        {nav ? <CloseIcon className="icon" /> : <MenuIcon className="icon" />}
+      </div>
+      <div className={nav ? "mobile__menu active" : "mobile__menu"}>
+        <ul className="mobile__nav">
           <li>Home</li>
           <li>Destinations</li>
           <li>Travel</li>
           <li>Views</li>
         </ul>
-        <div className="nav__icons">
-          <SearchIcon className='icon'style={{marginRight:'1rem'}} />
-          <PersonIcon className='icon'/>
-        </div>
-        <div className="hamburger" onClick={handleNav}>
-          {nav ? (<CloseIcon className='icon'/>) : (<MenuIcon className='icon' />)}
-        </div>
-        <div className={nav ? 'mobile__menu active' : "mobile__menu"}>
-          <ul className="mobile__nav">
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Travel</li>
-            <li>Views</li>
-          </ul>
-          <div className="mobile__menu__bottom">
-            <div className="menu__icons">
-              <button>Search</button>
-              <button>Account</button>
-            </div>
-            <div className="social__icons">
-              <Instagram className='icon'/>
-              <Facebook className='icon'/>
-              <YouTube className='icon'/>
-              <Twitter className='icon'/>
-            </div>
+        <div className="mobile__menu__bottom">
+          <div className="menu__icons">
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className="social__icons">
+            <Instagram className="icon" />
+            <Facebook className="icon" />
+            <YouTube className="icon" />
+            <Twitter className="icon" />
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
