024cfae5f2c5eea2dabf40373f085fc38a961217
NandoLofi <n.brighter00@gmail.com>
2022-12-17 15:31:40 -0600

    adjusted components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Destinations from "./components/destinations/Destinations";
 import Search from "./components/search/Search";
-import Selects from "./components/selects/Selects";
 
 function App() {
   return (
@@ -10,8 +9,6 @@ function App() {
       <Navbar />
       <Hero />
       <Destinations/>
-      <Search />
-      <Selects />
     </div>
   );
 }
--- src/components/destinations/Destinations.jsx
+++ src/components/destinations/Destinations.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import './Destinations.css'
-import Paris from '../../assets/paris.jpg'
-import Nyc from '../../assets/nyc.jpg'
-import Melbourne from '../../assets/melbourne.jpg'
-import London from '../../assets/london.jpg'
+import React from "react";
+import "./Destinations.css";
+import SelectsImg from "../selectsimg/SelectsImg";
+import Paris from "../../assets/paris.jpg";
+import Nyc from "../../assets/nyc.jpg";
+import Melbourne from "../../assets/melbourne.jpg";
+import London from "../../assets/london.jpg";
 
 export default function Destinations() {
   return (
-    <div className='destinations'>
-        <div className="container">
-            <h1> Tennis Open Host Cities </h1>
+    <div className="destinations">
+      <div className="container">
+        <h1> Tennis Open Host Cities </h1>
+      </div>
+      <div className="img__container">
+        <SelectsImg bgImg={London} text="London" />
+        <SelectsImg bgImg={Melbourne} text="Melbourne" />
+        <SelectsImg bgImg={Paris} text="Paris" />
+        <SelectsImg bgImg={Nyc} text="New York City" />
+      </div>
+      <div className="search__info">
+        <div className="left">
+          <h2>Enjoy the city and world-class matches!</h2>
+          <p>
+            While experiencing ambiance of a Tennis Major, enjoy the city that
+            host these memorable tournaments. You can visit the Louvre in Paris,
+            enjoy NY Style pizza first hand while in New York City, some
+            delicate seafood in Melbourne, or even some classica fish and chips
+            in London.
+          </p>
         </div>
-        <div className="img__container">
-            <img src={Paris} className='span__3 image__row__2' alt='parisimage'  />
-            <img src={Nyc} alt='NYCimage'/>
-            <img src={Melbourne}  alt='Melbourneimage'/>
-            <img src={London} className="london" alt='Londonimage'/>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
